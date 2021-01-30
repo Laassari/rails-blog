6a114ac3946d45b4c00c7f2f13b5bcb0cfcae013
Laassari Noureddine <nlaassari@gmail.com>
2021-01-30 23:46:27 +0100

    fix current_user for deleted users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def current_user
-    user ||= User.find(session[:user_id]) if session[:user_id]
+    user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     user
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def current_user
-    user ||= User.find(session[:user_id]) if session[:user_id]
+    user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     user
   end
 end
